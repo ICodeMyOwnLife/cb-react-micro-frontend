bb2e471a73a10e7c7858d863fc644c55a237ff53
ICodeMyOwnLife <icodemyownlife@gmail.com>
2020-04-24 11:20:11 +0700

    fix(types): export MicroFrontendAppProps and MicroFrontendRouteProps

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,3 +2,4 @@ export { default as MicroFrontend } from './MicroFrontend';
 export { default as MicroFrontendRoute } from './MicroFrontendRoute';
 export { default as MicroFrontendRoutes } from './MicroFrontendRoutes';
 export { default as bootstrapMicroFrontend } from './bootstrapMicroFrontend';
+export type { MicroFrontendAppProps, MicroFrontendRouteProps } from './types';
